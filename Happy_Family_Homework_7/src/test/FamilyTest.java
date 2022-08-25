c437d5d0f9f466e82f8fba7cd752c4d038dea1f8
Nazryn <80715402+NazrinTaghyeva@users.noreply.github.com>
2022-08-25 12:21:27 +0400

    Update FamilyTest.java

--- Happy_Family_Homework_7/src/test/FamilyTest.java
+++ Happy_Family_Homework_7/src/test/FamilyTest.java
@@ -1,24 +1,22 @@
 package test;
 
-import animals.Dog;
-import enums.AnimalSpeciesEnum;
+import enums.AnimaSpeciesEnum;
 import enums.DaysOfWeekEnum;
 import main.Family;
 import main.Human;
-import abstractClasses.Pet;
+import main.Pet;
 import org.testng.annotations.Test;
 
 import java.util.Locale;
 
 import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertNotEquals;
 
 public class FamilyTest {
-    Pet pet = new Dog(AnimalSpeciesEnum.DOG,"Cookie",2,84, new String[]{"eating","playing"});
+    Pet pet = new Pet(AnimaSpeciesEnum.DOG,"Cookie",2,84, new String[]{"eating","playing"});
     Human mother = new Human("Kate","Gomez" , 1975);
     Human father = new Human("John","Gomez" , 1970);
     Family family = new Family(mother,father);
-    Human child = new Human("Selena","Gomez",2002,85, pet, mother,father,new String[][]{{DaysOfWeekEnum.MONDAY.name().toLowerCase(Locale.ROOT),"meet the friends"},{DaysOfWeekEnum.SUNDAY.name().toLowerCase(Locale.ROOT),"watch the film"}},family);
-    Human child2 = new Human("Rosy","Gomez",2005,47,pet, mother,father,new String[][]{{DaysOfWeekEnum.FRIDAY.name().toLowerCase(Locale.ROOT),"meet the friends"},{DaysOfWeekEnum.THURSDAY.name().toLowerCase(Locale.ROOT),"watch the film"}},family);
 
 
     @Test
@@ -36,6 +34,8 @@ public class FamilyTest {
         Human child2 = new Human("Rosy","Gomez",2005,47,pet, mother,father,new String[][]{{DaysOfWeekEnum.FRIDAY.name().toLowerCase(Locale.ROOT),"meet the friends"},{DaysOfWeekEnum.THURSDAY.name().toLowerCase(Locale.ROOT),"watch the film"}},family);
         Family family = new Family(mother,father);
         family.addChild(child);
+        family.addChild(child2);
+        assertEquals(2, family.getChildren().length);
     }
 
     @Test
@@ -45,7 +45,10 @@ public class FamilyTest {
         Family family = new Family(mother,father);
         family.addChild(child);
         family.addChild(child2);
+        System.out.println(family.countFamily());
         family.deleteChild(0);
+        System.out.println(family.countFamily());
+        assertEquals(1,family.getChildren().length);
 
     }
 
